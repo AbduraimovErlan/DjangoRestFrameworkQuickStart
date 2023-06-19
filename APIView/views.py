f97dba0dcf38deafcac77b885eee6b3239b68e8e
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-19 16:00:51 -0400

    def initial(self, request, *args, **kwargs):

--- APIView/views.py
+++ APIView/views.py
@@ -400,3 +400,22 @@ def http_method_not_allowed_(self, request, *args, **kwargs):
         extra={"status_code": 405, "request": request},
     )
     return HttpResponseNotAllowed(self._allowed_methods())
+
+
+
+
+def initial(self, request, *args, **kwargs):
+    """
+    Runs anything that needs to occur prior to calling the method handler.
+    """
+    self.format_kwarg = self.get_format_suffix(**kwargs)
+    # Perform content negotiation and store the accepted info on the request
+    neg = self.perform_content_negotiation(request)
+    request.accepted_renderer, request.accepted_media_type = neg
+    # Determine th API version, if versioning is in user.
+    version, scheme = self.determine_version(request, *args, **kwargs)
+    request.version, request.versioning_scheme = version, scheme
+    # Ensure that the incoming request is permitted
+    self.perform_authentication(request)
+    self.check_permissions(request)
+    self.chech_throttles(request)
