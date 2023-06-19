adef70077eb553b1f352ed0b8d9c4a85e282342e
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-19 16:32:30 -0400

    def raise_uncaught_exception(self, exc):

--- APIView/views.py
+++ APIView/views.py
@@ -496,3 +496,13 @@ def permission_denied(self, request, message=None, code=None):
         raise exceptions.NotAuthenticated()
     raise exceptions.PermissionDenied(detail=message, code=code)
 
+
+
+
+def raise_uncaught_exception(self, exc):
+    if settings.DEBUG:
+        request = self.request
+        renderer_format = getattr(request.accepted_renderer, 'format')
+        use_plaintext_traceback = renderer_format not in ('html', 'api', 'admin')
+        request.force_plaintext_errors(use_plaintext_traceback)
+    raise exc
